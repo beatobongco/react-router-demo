327a14a59147e8c10262d23c3ca61617eeed8c3a
Beato Bongco <beatobongco@gmail.com>
2018-10-31 15:29:40 +0800

    Remove build/ from gitignore and add build folder to repo

---
+++ build/precache-manifest.b59695077eeb05f377677f9e8e1eea9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93594614685006bf7720",
+    "url": "/static/js/main.93594614.chunk.js"
+  },
+  {
+    "revision": "889475eb32a530b4d56b",
+    "url": "/static/js/1.889475eb.chunk.js"
+  },
+  {
+    "revision": "93594614685006bf7720",
+    "url": "/static/css/main.84aefe35.chunk.css"
+  },
+  {
+    "revision": "a996601be020943c9ecf7dd72c38df72",
+    "url": "/index.html"
+  }
+];
